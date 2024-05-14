a0f09522d057229f7e01eb31172742b99e84b833
Guillermo Serrano <gserrano@paloaltonetworks.com>
2024-05-14 12:24:25 +0100

    initial commit

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "rg" {
+  type = string
+}
+
+variable "region1" {
+  type = string
+}
+
+variable "region2" {
+  type = string
+}
+
+variable "vhub-us" {
+  type = string
+}
+
+variable "address-vhub-us" {
+  type = string
+}
+
+variable "address-vhub-eu" {
+  type = string
+}
+
+variable "vhub-eu" {
+  type = string
+}
+
+variable "panorama-string" {
+  type = string
+}
